50385d014ecdc7a9e63746fec52a4428de6c6d15
Aaryan <sonofnaleen@gmail.com>
2024-03-19 10:57:11 +0545

    routes set up

--- task1-portfolio-website/src/App.js
+++ task1-portfolio-website/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import { AllRoute } from "./routes/AllRoute";
 
 function App() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Header />
+      <AllRoute />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ task1-portfolio-website/src/routes/AllRoute.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AllRoute = () => {
+  return <div>AllRoute</div>;
+};
