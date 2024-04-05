22e032532a6559c63a5608839e199c927e2e85e8
Aaryan <sonofnaleen@gmail.com>
2024-04-05 11:04:03 +0545

    Header Completed

---
+++ task-3-task_tracker_application/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../assets/company_logo.png";
+
+export const Header = () => {
+  return (
+    <center>
+      <header>
+        <div className="logo">
+          <img src={Logo} alt="Task Tracker Logo" />
+          <span>Task Tracker</span>
+        </div>
+      </header>
+    </center>
+  );
+};
